dba595f72654873d07edde7c7dfdf3feab4395cd
psklrenaud <wcs@pascal-renaud.com>
2018-11-29 17:09:17 +0100

    commit nouveau repo

--- src/app/acceuil/acceuil.component.ts
+++ src/app/acceuil/acceuil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+declare var TweenMax:any;
+declare var TimelineLite:any;
 
 @Component({
   selector: 'app-acceuil',
@@ -10,6 +12,19 @@ export class AcceuilComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //bouton decouvrir
+    const discover = document.getElementById("discover");
+    const presentation = document.getElementById("presentation");
+    discover.addEventListener("click", function() {
+      presentation.classList.remove("hidden");
+    });
   }
 
-}
+  goDown() {
+    let pos = document.getElementById("presentation");
+    let presentation= document.getElementById("presentation");
+    let tl = new TimelineLite();
+    tl.from(pos, 1)
+    .to(presentation, 1);
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcceuilComponent
+    AcceuilComponent,
+    PresentationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserModule, 
+    BrowserAnimationsModule,
+
+    RouterModule.forRoot(
+      [
+
+      { path: "accueil", component: AcceuilComponent, pathMatch:"full" },
+      { path: "presentation", component: PresentationComponent, pathMatch:"full" },
+   
+      { path: "", redirectTo:"accueil", pathMatch:"full"}
+
+
+      ],
+      
+      {useHash: true}
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
